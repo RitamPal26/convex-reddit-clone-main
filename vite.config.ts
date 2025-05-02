531c9cd20b854e3abdd00869be57aa48eca8ccd1
Ritam Pal <ritamjunior26@gmail.com>
2025-05-02 12:45:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: [
+      'a6e1ed86becdc474aa98c30ea66f63c4-359446609.eu-north-1.elb.amazonaws.com'
+    ]
   }
 })
