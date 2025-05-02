3313e9428be0d35819295e06e7b6b262525fafd5
Ritam Pal <ritamjunior26@gmail.com>
2025-05-02 14:47:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
     allowedHosts: [
-      'a6e1ed86becdc474aa98c30ea66f63c4-359446609.eu-north-1.elb.amazonaws.com'
+      'a6e1ed86becdc474aa98c30ea66f63c4-359446609.eu-north-1.elb.amazonaws.com:3000'
     ]
   }
 })
